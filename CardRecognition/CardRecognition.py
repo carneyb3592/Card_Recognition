39e2f9a681ae75f93627c649fe04b3e580431354
Ben Carney <ben.carney3@gmail.com>
2021-03-23 14:44:57 -0700

    Drew Contours

--- CardRecognition/CardRecognition.py
+++ CardRecognition/CardRecognition.py
@@ -10,14 +10,23 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
     #frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
+
+
+
 
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #cv2.imshow('Gray',grayImage)
 
     cannyN = cv2.Canny(frame, 150, 175)
     cv2.imshow('Canny Normal', cannyN)
+    ret, thresh = cv2.threshold(cannyN, 127, 255, 0)
+   
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #print(hierarchy)    
+    cv2.drawContours(frame, contours, -1, (0,255,0), 0)
+    cv2.imshow('Input', frame)
     c = cv2.waitKey(1)
+    #print("Number of Contours found = " + str(len(contours)))
     if c == 27:
         
         break
