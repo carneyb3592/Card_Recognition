fd8ee43806f41719ba8f22445c7d5519ed8d850d
Ben Carney <ben.carney3@gmail.com>
2021-03-31 18:18:02 -0700

    Added Card images and made flattening method

--- CardRecognition/CardRecognition.py
+++ CardRecognition/CardRecognition.py
@@ -1,6 +1,8 @@
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
+import os
+os.chdir(r'C:\Users\djntr\source\repos\CardRecognition\CardRecognition')
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 # Check if the webcam is opened correctly
@@ -8,7 +10,7 @@ if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
 CARD_RATIO = 0.63
-CARD_AREA_MIN = 25000
+CARD_AREA_MIN = 70000
 CARD_AREA_MAX = 100000
 SUIT_AREA_MIN = 200
 SUIT_AREA_MAX = 5000
@@ -19,9 +21,16 @@ class Card:
         self.height = 0
         self.x = 0
         self.y = 0
+        self.rankImg = []
         self.rank = ""
+        self.suitImg = []
         self.suit = ""
+        self.valuesOnCard = []
+        self.correctedImg = []
 
+class Suits:
+    def __init__(self):
+        self.width = 0
 
 def detect_Cards(contours, hierarchy, frame):
     possible_cards = []
@@ -51,21 +60,75 @@ def detect_Cards(contours, hierarchy, frame):
             thickness = 2
    
             # Using cv2.putText() method
-            image = cv2.putText(frame, 'Card', (x + int(w/2),y + int(h/2)), font, 
-                               fontScale, color, thickness, cv2.LINE_AA)
+            #image = cv2.putText(frame, 'Card', (x + int(w/2),y + int(h/2)), font, 
+            #                  fontScale, color, thickness, cv2.LINE_AA)
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
 
-    recognize_cards(possible_cards,contours, hierarchy, frame)
+    recognize_cards(possible_cards, frame,contours,hierarchy)
     
 
-def recognize_cards(cards,contours, hierarchy, frame):
-    for contour in contours:
-        (x,y,w,h) = cv2.boundingRect(contour)
-        area = w * h
-        if area < SUIT_AREA_MAX and area > SUIT_AREA_MIN:
-            for card in cards:
-                if(x > card.x and x < card.x+card.width and y > card.y and y < card.y+card.height):
-                    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+def recognize_cards(cards, frame,contours,hierarchy):
+    for card in cards:
+        card.correctedImg = makeCardReadable(card, frame)
+        cv2.imshow("cropped", card.correctedImg)
+        card.rankImg = cv2.resize(card.correctedImg[10:50,10:30],(40,80))
+        cv2.imshow("ranking", card.rankImg)
+        card.suitImg = cv2.resize(card.correctedImg[50:80,10:30],(60,90))
+        cv2.imshow("suit", card.suitImg)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.imwrite("King.jpg",card.rankImg)
+
+
+      
+     
+def makeCardReadable(card, frame):
+
+    "Sourced from https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"
+    rect = np.zeros((4, 2), dtype = "float32")
+
+    # List of coordinats of card from Top Left, Top Right, Bottom Left, Bottom Right
+    pts = np.float32([(card.x,card.y),(card.x, card.y + card.height),(card.x + card.width, card.y),(card.x + card.width, card.y + card.height)])
+
+
+
+    if(card.width < card.height):
+        s = pts.sum(axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+
+        difference = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(difference)]
+        rect[3] = pts[np.argmax(difference)]
+    
+
+    if(card.width > card.height):
+        s = pts.sum(axis = 1)
+        rect[1] = pts[np.argmin(s)]
+        rect[3] = pts[np.argmax(s)]
+
+        difference = np.diff(pts, axis = 1)
+        rect[2] = pts[np.argmin(difference)]
+        rect[0] = pts[np.argmax(difference)]
+
+    (tl, tr, br, bl) = rect
+    
+   
+    maxWidth = 200
+    
+    maxHeight = 300
+
+    dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	# compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(frame, M, (maxWidth, maxHeight))
+    gray_warped =  cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+	# return the warped image
+    return gray_warped
+
 
 
 while True:
@@ -73,13 +136,12 @@ while True:
     #frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
 
-
-
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #cv2.imshow('Gray',grayImage)
 
     cannyN = cv2.Canny(frame, 150, 175)
-    cv2.imshow('Canny Normal', cannyN)
+    
+    #cv2.imshow('Canny Normal', cannyN)
     ret, thresh = cv2.threshold(cannyN, 127, 255, 0)
    
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
