50095998c2e8f3f5426737ac4c086ff0eabd14e3
Ben Carney <ben.carney3@gmail.com>
2021-04-04 00:10:12 -0700

    Kinda made image recgonition

--- CardRecognition/CardRecognition.py
+++ CardRecognition/CardRecognition.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 # Check if the webcam is opened correctly
 if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+    raise IOError('Cannot open webcam')
 
 CARD_RATIO = 0.63
 CARD_AREA_MIN = 25000
@@ -27,45 +27,46 @@ class Card:
         self.x = 0
         self.y = 0
         self.rankImg = []
-        self.rank = ""
+        self.rank = ''
         self.value = 0
         self.suitImg = []
-        self.suit = ""
+        self.suit = ''
         self.valuesOnCard = []
         self.correctedImg = []
 
 class Suit:
     def __init__(self):
        self.image = []
-       self.name = ""
+       self.name = ''
 
 class Value:
     def __init__(self):
+       
        self.image = []
-       self.name = ""
+       self.name = ''
        self.value = 0
 
 def process_suits():
     suit_list = []
     counter = 0
 
-    for Suits in ["Clubs","Diamonds","Spades","Hearts"]:
+    for Suits in ['Clubs','Diamonds','Spades','Hearts']:
         suit_list.append(Suit())
         suit_list[counter].name = Suits
-        suit_list[counter].image = cv2.resize(cv2.imread((FILEPATH + Suits + ".jpg")),(60,90))
-        counter += 1
+        suit_list[counter].image = cv2.resize(cv2.imread((FILEPATH + Suits + '.jpg'),cv2.COLOR_BGR2GRAY),(60,90))
+        counter = counter + 1
     return suit_list
 def process_values():
 
     value_list = []
     counter = 0
 
-    for Values in ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]:
+    for Values in ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King']:
         value_list.append(Value())
         value_list[counter].name = Values
-        value_list[counter].image = cv2.resize(cv2.imread((FILEPATH + Values + ".jpg")),(40,80))
+        value_list[counter].image = cv2.resize(cv2.imread((FILEPATH + Values + '.jpg'),cv2.COLOR_BGR2GRAY),(40,80))
         value_list[counter].value = counter + 1
-        counter + 1
+        counter = counter + 1
     return value_list
 
 def detect_Cards(contours, frame):
@@ -126,18 +127,28 @@ def recognize_cards(cards, frame,contours,hierarchy):
 
     for card in cards:
         card.correctedImg = makeCardReadable(card, frame)
+        cv2.imshow('Rcards',card.correctedImg)
         card.rankImg = cv2.resize(card.correctedImg[0:45,0:32],(40,80))
+        cv2.imshow('Rank',card.rankImg)
         card.suitImg = cv2.resize(card.correctedImg[45:84,0:32],(60,90))
+        cv2.imshow('Suit',card.suitImg)
+        c = cv2.waitKey(1)
+        if c == ord('q'):
+            cv2.imwrite("Four.jpg",card.rankImg)
+            cv2.imwrite("Clubs.jpg",card.suitImg)
+
         #card.suit = detect_suit(card,suits)
         #card.rank, card.value = detect_value(card,values)
         #print(card.suit,card.rank)
 
 def detect_suit(card,suits):
     best_match = 10000
-    best_name = "Undetermined"
+    best_name = 'Undetermined'
 
     for suit in suits:
+        
         diff = cv2.absdiff(card.suitImg,suit.image)
+      
         diff_value = int(np.sum(diff)/255)
 
         if diff_value < best_match:
@@ -147,21 +158,23 @@ def detect_suit(card,suits):
 
 def detect_value(card,values):
     best_match = 10000
-    best_name = "Undetermined"
+    best_name = 'Undetermined'
     best_value = 0
-    for Value in values:
+    for value in values:
+        
         diff = cv2.absdiff(card.rankImg,value.image)
+
         diff_value = int(np.sum(diff)/255)
 
         if diff_value < best_match:
             best_match = diff_value
-            best_name = Value.name
-            best_value = Value.value
+            best_name = value.name
+            best_value = value.value
     return best_name, best_value
 def makeCardReadable(card, frame):
 
-    "Sourced from https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"
-    rect = np.zeros((4, 2), dtype = "float32")
+    'Sourced from https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/'
+    rect = np.zeros((4, 2), dtype = 'float32')
     s = card.corners.sum(axis = 2)
 
     if(card.width < card.height):
@@ -191,13 +204,15 @@ def makeCardReadable(card, frame):
 		[0, 0],
 		[maxWidth - 1, 0],
 		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
+		[0, maxHeight - 1]], dtype = 'float32')
 	# compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(frame, M, (maxWidth, maxHeight))
     gray_warped =  cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
 	# return the warped image
     return gray_warped
+
+
 
 
 
@@ -222,7 +237,7 @@ while True:
     #cv2.drawContours(frame, contours, -1, (0,255,0), 0)
     cv2.imshow('Input', frame)
     c = cv2.waitKey(1)
-    #print("Number of Contours found = " + str(len(contours)))
+    #print('Number of Contours found = ' + str(len(contours)))
     if c == 27:
         
         break
