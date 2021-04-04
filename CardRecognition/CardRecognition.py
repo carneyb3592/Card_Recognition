c8fb3784fe91db30060509bfad8722a0772e8825
Ben Carney <ben.carney3@gmail.com>
2021-04-03 20:07:40 -0700

    Improved Detection, Now loads suits and values.

--- CardRecognition/CardRecognition.py
+++ CardRecognition/CardRecognition.py
@@ -10,12 +10,17 @@ if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
 CARD_RATIO = 0.63
-CARD_AREA_MIN = 70000
-CARD_AREA_MAX = 100000
+CARD_AREA_MIN = 25000
+CARD_AREA_MAX = 80000
 SUIT_AREA_MIN = 200
 SUIT_AREA_MAX = 5000
+FILEPATH = r'C:\Users\djntr\source\repos\CardRecognition\CardRecognition\CardImages\\'
+
+
 class Card:
     def __init__(self):
+        self.corners = []
+        self.contour = []
         self.boundingRect = []
         self.width = 0
         self.height = 0
@@ -23,92 +28,157 @@ class Card:
         self.y = 0
         self.rankImg = []
         self.rank = ""
+        self.value = 0
         self.suitImg = []
         self.suit = ""
         self.valuesOnCard = []
         self.correctedImg = []
 
-class Suits:
+class Suit:
     def __init__(self):
-        self.width = 0
+       self.image = []
+       self.name = ""
 
-def detect_Cards(contours, hierarchy, frame):
+class Value:
+    def __init__(self):
+       self.image = []
+       self.name = ""
+       self.value = 0
+
+def process_suits():
+    suit_list = []
+    counter = 0
+
+    for Suits in ["Clubs","Diamonds","Spades","Hearts"]:
+        suit_list.append(Suit())
+        suit_list[counter].name = Suits
+        suit_list[counter].image = cv2.resize(cv2.imread((FILEPATH + Suits + ".jpg")),(60,90))
+        counter += 1
+    return suit_list
+def process_values():
+
+    value_list = []
+    counter = 0
+
+    for Values in ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]:
+        value_list.append(Value())
+        value_list[counter].name = Values
+        value_list[counter].image = cv2.resize(cv2.imread((FILEPATH + Values + ".jpg")),(40,80))
+        value_list[counter].value = counter + 1
+        counter + 1
+    return value_list
+
+def detect_Cards(contours, frame):
     possible_cards = []
-    for contour in contours:
-        (x,y,w,h) = cv2.boundingRect(contour)
-        area = w * h
-        areaActual = cv2.contourArea(contour)
-        extent = float(areaActual)/area
-        #ratio_of_width_to_height = w/h
-        if area < CARD_AREA_MAX and area > CARD_AREA_MIN  and extent > 0.7:
-            card = Card()
-            card.boundingRect = [x,y,w,h]
-            card.width = w
-            card.height = h
-            card.x = x
-            card.y = y
-            possible_cards.append(card)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-  
-            # fontScale
-            fontScale = 1
-   
-            # Blue color in BGR
-            color = (255, 0, 0)
-  
-            # Line thickness of 2 px
-            thickness = 2
-   
-            # Using cv2.putText() method
-            #image = cv2.putText(frame, 'Card', (x + int(w/2),y + int(h/2)), font, 
-            #                  fontScale, color, thickness, cv2.LINE_AA)
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    for contour in contours: 
+        insideCard = False
 
+        (x,y,w,h) = cv2.boundingRect(contour)
+        #area = w * h
+
+        areaActual = cv2.contourArea(contour)
+        # Finds number of sides of the object
+        sides = cv2.approxPolyDP(contour, 0.1* cv2.arcLength(contour,True) ,True)
+        #extent = float(areaActual)/area
+        #ratio_of_width_to_height = w/h
+        if areaActual < CARD_AREA_MAX and areaActual > CARD_AREA_MIN  and len(sides) == 4:
+            
+            #Checks for cases where face cards have boxes inside that would normally come up as a card
+            if len(possible_cards) != 0:
+                for card in possible_cards:
+                    if x > card.x and x < card.x+card.width and y > card.y and y < card.y + card.height:
+                        insideCard = True
+            if not insideCard:
+                
+                corners = np.float32(sides)
+                card = Card()
+                card.contour = contour
+                card.boundingRect = [x,y,w,h]
+                card.width = w
+                card.height = h
+                card.x = x
+                card.y = y
+                card.corners = corners
+                possible_cards.append(card)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+  
+                # fontScale
+                fontScale = 1
+   
+                # Blue color in BGR
+                color = (255, 0, 0)
+  
+                # Line thickness of 2 px
+                thickness = 2
+   
+                # Using cv2.putText() method
+                #image = cv2.putText(frame, 'Card', (x + int(w/2),y + int(h/2)), font, 
+                #                  fontScale, color, thickness, cv2.LINE_AA)
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+                #cv2.drawContours(frame, contour, -1, (0,255,0), 2)
+                #print(card.contour)
     recognize_cards(possible_cards, frame,contours,hierarchy)
     
 
 def recognize_cards(cards, frame,contours,hierarchy):
+    suits = process_suits()
+    values = process_values()
+
     for card in cards:
         card.correctedImg = makeCardReadable(card, frame)
-        cv2.imshow("cropped", card.correctedImg)
-        card.rankImg = cv2.resize(card.correctedImg[10:50,10:30],(40,80))
-        cv2.imshow("ranking", card.rankImg)
-        card.suitImg = cv2.resize(card.correctedImg[50:80,10:30],(60,90))
-        cv2.imshow("suit", card.suitImg)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.imwrite("King.jpg",card.rankImg)
+        card.rankImg = cv2.resize(card.correctedImg[0:45,0:32],(40,80))
+        card.suitImg = cv2.resize(card.correctedImg[45:84,0:32],(60,90))
+        #card.suit = detect_suit(card,suits)
+        #card.rank, card.value = detect_value(card,values)
+        #print(card.suit,card.rank)
 
+def detect_suit(card,suits):
+    best_match = 10000
+    best_name = "Undetermined"
 
-      
-     
+    for suit in suits:
+        diff = cv2.absdiff(card.suitImg,suit.image)
+        diff_value = int(np.sum(diff)/255)
+
+        if diff_value < best_match:
+            best_match = diff_value
+            best_name = suit.name
+    return best_name
+
+def detect_value(card,values):
+    best_match = 10000
+    best_name = "Undetermined"
+    best_value = 0
+    for Value in values:
+        diff = cv2.absdiff(card.rankImg,value.image)
+        diff_value = int(np.sum(diff)/255)
+
+        if diff_value < best_match:
+            best_match = diff_value
+            best_name = Value.name
+            best_value = Value.value
+    return best_name, best_value
 def makeCardReadable(card, frame):
 
     "Sourced from https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"
     rect = np.zeros((4, 2), dtype = "float32")
-
-    # List of coordinats of card from Top Left, Top Right, Bottom Left, Bottom Right
-    pts = np.float32([(card.x,card.y),(card.x, card.y + card.height),(card.x + card.width, card.y),(card.x + card.width, card.y + card.height)])
-
-
+    s = card.corners.sum(axis = 2)
 
     if(card.width < card.height):
-        s = pts.sum(axis = 1)
-        rect[0] = pts[np.argmin(s)]
-        rect[2] = pts[np.argmax(s)]
-
-        difference = np.diff(pts, axis = 1)
-        rect[1] = pts[np.argmin(difference)]
-        rect[3] = pts[np.argmax(difference)]
+        rect[0] = card.corners[np.argmin(s)]
+        rect[2] = card.corners[np.argmax(s)]
+        difference = np.diff(card.corners, axis = 2)
+        rect[1] = card.corners[np.argmin(difference)]
+        rect[3] = card.corners[np.argmax(difference)]
     
 
     if(card.width > card.height):
-        s = pts.sum(axis = 1)
-        rect[1] = pts[np.argmin(s)]
-        rect[3] = pts[np.argmax(s)]
+        rect[1] = card.corners[np.argmin(s)]
+        rect[3] = card.corners[np.argmax(s)]
 
-        difference = np.diff(pts, axis = 1)
-        rect[2] = pts[np.argmin(difference)]
-        rect[0] = pts[np.argmax(difference)]
+        difference = np.diff(card.corners, axis = 2)
+        rect[2] = card.corners[np.argmin(difference)]
+        rect[0] = card.corners[np.argmax(difference)]
 
     (tl, tr, br, bl) = rect
     
@@ -145,7 +215,9 @@ while True:
     ret, thresh = cv2.threshold(cannyN, 127, 255, 0)
    
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    detect_Cards(contours, hierarchy,frame)
+    
+    detect_Cards(contours, frame)
+    
     #print(hierarchy)    
     #cv2.drawContours(frame, contours, -1, (0,255,0), 0)
     cv2.imshow('Input', frame)
